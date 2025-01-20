7f1c560658f0f299cce68141b0c9bbb45873f21d
Jcliz <joaopedrocliz@gmail.com>
2025-01-20 14:16:11 -0300

    Testes de layout

--- src/com/github/jcliz/calc/visao/Botao.java
+++ src/com/github/jcliz/calc/visao/Botao.java
@@ -1,7 +1,15 @@
 package com.github.jcliz.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Botao extends JButton {
-
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setFont(new Font("courier", Font.PLAIN, 25));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setForeground(Color.WHITE);
+    }
 }
